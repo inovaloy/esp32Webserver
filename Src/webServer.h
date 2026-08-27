defe3d107104ce46fd7f0b088e782255ef90e418
Arghya Biswas <arghya.biswas1@ibm.com>
2026-08-28 00:15:13 +0530

    add some new api
    Signed-off-by: Arghya Biswas <arghyabiswas05@gmail.com>

--- Src/webServer.h
+++ Src/webServer.h
@@ -1,15 +1,9 @@
 #ifndef WEB_SERVER_H
 #define WEB_SERVER_H
 
+// Hook implementations live in webServer.cpp.
+// The hook signatures themselves are declared in AutoGen/autoGenWebServer.h
+// (generated from linkerData.yaml). Do not redeclare them here.
 #include "AutoGen/autoGenWebServer.h"
-
-void webHandlerHook(webServerMacro hook);
-
-// Hooks for API handlers
-char* apiLoginHandlerHook(httpd_req_t *req);
-char* apiRegisterHandlerHook(httpd_req_t *req);
-char* apiWifiStatusHandlerHook(httpd_req_t *req);
-char* apiWifiScanHandlerHook(httpd_req_t *req);
-char* apiWifiConnectHandlerHook(httpd_req_t *req);
 
 #endif // WEB_SERVER_H
